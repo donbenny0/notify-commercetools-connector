b26ae2b19c57f32d83d9bf834250da9483261772
Don Benny <don.benny@ayatacommerce.com>
2025-04-14 17:20:10 +0530

    feat: restructure project by removing obsolete files, adding new configuration, and implementing lazy loading for components

--- merchant-center-notify/src/components/pages/configuration/index.ts
+++
@@ -1,7 +0,0 @@
-import { lazy } from 'react';
-
-const ConfigurationPage = lazy(
-    () => import('./configurationPage' /* webpackChunkName: "editMessages" */)
-);
-
-export default ConfigurationPage;
--- merchant-center-notify/src/components/ui-components/loader/index.ts
+++
@@ -1,7 +0,0 @@
-import { lazy } from 'react';
-
-const Loader = lazy(
-  () => import('./loader' /* webpackChunkName: "welcome" */)
-);
-
-export default Loader;
